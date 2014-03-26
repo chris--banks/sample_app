ce5be1bd00ab35c33390ee9714615b7ea1154282
Chris Banks <git@chris-banks.co.uk>
2014-03-26 22:12:06 +0000

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,8 +4,6 @@ describe "Static pages" do
 
   subject { page }
 
-  subject { page }
-
   shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
     it { should have_title(full_title(page_title)) }
